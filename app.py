f72ef7b62b6780bf3eeb084ada48b8445abc1c93
DUCK5646 <155681610+DUCK5646@users.noreply.github.com>
2024-01-12 10:09:14 -0800

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def grid():
+    return render_template("index.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+@app.route('/contact')
+def contact():
+    return render_template("contact.html")
+@app.route('/products')
+def product():
+    return render_template("product.html")
+
+if __name__ == '__main__':
+    app.run(debug=True)
