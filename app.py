83f31f4d3bd46444328ff8a45c1851c37505b852
DUCK5646 <155681610+DUCK5646@users.noreply.github.com>
2024-05-13 13:42:41 -0700

    Website Framework with lyric generator

--- app.py
+++ app.py
@@ -1,31 +1,32 @@
+import json
 from flask import Flask, render_template, request
-
+import os
+from openai import OpenAI
 app = Flask(__name__)
+client= OpenAI()
 
 @app.route('/')
 def grid():
     return render_template("index.html")
-
 @app.route('/about')
 def about():
     return render_template("about.html")
-@app.route('/contact')
+@app.route('/lyrics')
 def contact():
-    return render_template("contact.html")
-@app.route('/products')
+    return render_template("lyrics.html")
+@app.route('/music')
 def product():
-    return render_template("product.html")
-@app.route('/confirmation')
-def confirmation():
-    name = request.args.get("name")
-    email = request.args.get("email")
-    props = {
-        "name": name,
-        "email": email
+    return render_template("music.html")
+@app.route('/chat')
+def chat_completion():
+    prompt = request.args.get('prompt')
+    messages = json.loads(prompt)
+    completion = client.chat.completions.create(
+        model="gpt-3.5-turbo",
+        messages=messages
 
-    }
-    return render_template( "confirmation.html", data=props)
-
-
+    )
+   # print(completion.choices[0].message)
+    return str(completion.choices[0].message.content)
 if __name__ == '__main__':
     app.run(debug=True)
